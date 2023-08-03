50ea2af2d2d76d33bffd1b9196d6940d788e95c0
samliburd <liburdsam@gmail.com>
2023-08-03 21:50:16 +0100

    Changed styling

--- src/components/passwordreset/passwordreset.tsx
+++ src/components/passwordreset/passwordreset.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import styles from './passwordreset.module.css'
+import styles from './passwordreset.module.scss'
 import UserDetails from '../userdetails/userdetails'
 import UserForm from '../userform/userform'
 
@@ -11,6 +11,12 @@ function PasswordReset() {
   return (
     <div className={styles.PasswordReset}>
       <h1>Password Reset Boilerplate</h1>
+      <ul className={styles.infoText}>
+        <li>Enter the user's first name and email and the text should automatically update.</li>
+        <li>Single clicking on either the email or password should highlight it for ease of copying.</li>
+        <li>The copy button at the bottom will set the clipboard to the boilerplate text <strong>with formatting. </strong></li>
+        <li>When pasting into Outlook make sure to select <i>"Match destination formatting"</i>.</li>
+      </ul>
       <UserForm setEmail={setEmail} setFirstName={setFirstName} />
       <UserDetails email={email} firstName={firstName} />
 
