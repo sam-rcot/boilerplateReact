48cb8237b277865ca4eda30f3b460586b9cb6766
samliburd <liburdsam@gmail.com>
2023-08-22 18:09:16 +0100

    Changed text spacing

--- src/components/userdetails/userdetails.tsx
+++ src/components/userdetails/userdetails.tsx
@@ -13,8 +13,8 @@ const UserDetails = ({ email, firstName }: UserDetailsProps) => {
     const resultRef = useRef<HTMLDivElement>(null);
     const [password, setPassword] = useState(generatePassword());
 
-    
-    
+
+
     const [pressedButtonId, setPressedButtonId] = useState<null | string>(null);
 
     const handleButtonPress = (id: string) => {
@@ -29,9 +29,10 @@ const UserDetails = ({ email, firstName }: UserDetailsProps) => {
     return (
         <>
             <div className={styles.UserDetails}>
-                <div className={styles.resultContainer} ref={resultRef} style={{fontFamily: 'Arial'}}>
-                    <p>Hi{firstName ? ` ${firstName.charAt(0).toUpperCase() + firstName.slice(1)}` : ""},</p>
-                    <p>
+                <div className={styles.resultContainer} ref={resultRef} style={{ fontFamily: 'Arial' }}>
+                    <p>Hi{firstName ? ` ${firstName.charAt(0).toUpperCase() + firstName.slice(1)}` : ""},<br /><br />
+
+
                         Apologies for the issues you have been having logging in. I have now
                         updated your account and created a new temporary password.
                         <br /><br />
@@ -42,14 +43,13 @@ const UserDetails = ({ email, firstName }: UserDetailsProps) => {
                         (https://portal.rcot.co.uk/) on a fresh web browser using the access
                         credentials shown below and amend your password to something more
                         secure.
-                    </p>
-                    
-                    <strong>Username:</strong> <span className={styles.select}>{email}</span>
-                    <br />
-                    <strong>Temporary password:</strong>{" "}
-                    <span className={styles.select}>{password}</span>
-                    <br />
-                    <p>Kind Regards,<br />
+                        <br /><br />
+                        <strong>Username:</strong> <span className={styles.select}>{email}</span>
+                        <br />
+                        <strong>Temporary password:</strong>{" "}
+                        <span className={styles.select}>{password}</span>
+                        <br /><br />
+                        Kind Regards,<br />
                         Digital Team</p>
                 </div>
                 <div className={styles.buttonContainer}>
@@ -66,7 +66,7 @@ const UserDetails = ({ email, firstName }: UserDetailsProps) => {
                         setPassword={setPassword}
                     />
                 </div>
-                
+
             </div>
         </>
     );
