1834d50adb2ac7a47718fe46e4a3b2d6196ae0cb
Sam Liburd <sam.liburd@rcot.co.uk>
2024-01-15 16:47:45 +0000

    Changed navbar style

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,47 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './navbar.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
-    return (
-        <div className={styles.NavBar}>
-            <ul className={styles.links}>
-                <li className={styles.link}><NavLink to="/" className={({ isActive }) => isActive ? styles.active : ''}>Password Reset</NavLink></li>
-                <li className={styles.link}><NavLink to="/generalpurpose" className={({ isActive }) => isActive ? styles.active : ''}>General Purpose</NavLink></li>
-                <li className={styles.link}><NavLink to="/inboxmonitoring" className={({ isActive }) => isActive ? styles.active : ''}>Inbox Monitoring</NavLink></li>
-                <li className={styles.link}><NavLink to="/newuser" className={({ isActive }) => isActive ? styles.active : ''}>New User</NavLink></li>
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const location = useLocation();
+    const getActivePageName = () => {
+        switch (location.pathname) {
+            case '/': return 'Password Reset';
+            case '/generalpurpose': return 'General Purpose';
+            case '/inboxmonitoring': return 'Inbox Monitoring';
+            case '/newuser': return 'New User';
+            default: return 'Menu';
+        }
+    };
 
-            </ul>
-        </div>
+    return (
+        <>
+            <div className={styles.NavBar}>
+                
+                <span className={styles.menu} onClick={() => setDropdownVisible(!dropdownVisible)}>
+                    {/*<span className={`${styles.menuText} ${dropdownVisible ? styles.noUnderline : ''}`}>{getActivePageName()}</span>*/}
+                    <span className={`${styles.menuText} ${dropdownVisible ? styles.hiddenMenu : ''}`}>{getActivePageName()}</span>
+                    <FontAwesomeIcon
+                        className={styles.faIcon}
+                        icon={dropdownVisible ? faChevronRight : faBars}
+                    />
+                </span>
+                <ul className={`${styles.links} ${dropdownVisible ? styles.visible : styles.hidden}`}>
+                    <li className={styles.link}><NavLink to="/" className={({ isActive }) => isActive ? styles.active : ''}>Password Reset</NavLink></li>
+                    <li className={styles.link}><NavLink to="/generalpurpose" className={({ isActive }) => isActive ? styles.active : ''}>General Purpose</NavLink></li>
+                    <li className={styles.link}><NavLink to="/inboxmonitoring" className={({ isActive }) => isActive ? styles.active : ''}>Inbox Monitoring</NavLink></li>
+                    <li className={styles.link}><NavLink to="/newuser" className={({ isActive }) => isActive ? styles.active : ''}>New User</NavLink></li>
+                </ul>
+            </div>
+
+        </>
     )
 }
 
